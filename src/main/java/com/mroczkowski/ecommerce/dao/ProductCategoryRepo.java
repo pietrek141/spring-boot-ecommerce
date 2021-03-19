da706d00ba284161fa80df48ce5bfa88dc4f9ec6
Piotr Mroczkowski <pietrek141@o2.pl>
2021-03-19 13:38:35 +0100

    upd

--- src/main/java/com/mroczkowski/ecommerce/dao/ProductCategoryRepo.java
+++ src/main/java/com/mroczkowski/ecommerce/dao/ProductCategoryRepo.java
@@ -3,7 +3,9 @@ package com.mroczkowski.ecommerce.dao;
 import com.mroczkowski.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("https://angular-ecommerce-frontend.herokuapp.com")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepo extends JpaRepository<ProductCategory, Long> {
 }
--- src/main/java/com/mroczkowski/ecommerce/dao/ProductRepo.java
+++ src/main/java/com/mroczkowski/ecommerce/dao/ProductRepo.java
@@ -5,8 +5,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@CrossOrigin("https://angular-ecommerce-frontend.herokuapp.com")
 @RepositoryRestResource
 public interface ProductRepo extends JpaRepository<Product, Long> {
 
